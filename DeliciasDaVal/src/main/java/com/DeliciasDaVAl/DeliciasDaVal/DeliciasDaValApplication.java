615b0a111039144effbfa0e9437dc586f31ff4e4
maluel <57541772+maluel@users.noreply.github.com>
2019-11-12 09:09:19 -0300

    Adicionei arquivos

---
+++ DeliciasDaVal/src/main/java/com/DeliciasDaVAl/DeliciasDaVal/DeliciasDaValApplication.java
@@ -0,0 +1,13 @@
+package com.DeliciasDaVAl.DeliciasDaVal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliciasDaValApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliciasDaValApplication.class, args);
+	}
+
+}
---
+++ DeliciasDaVal/src/test/java/com/DeliciasDaVAl/DeliciasDaVal/DeliciasDaValApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DeliciasDaVAl.DeliciasDaVal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciasDaValApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
